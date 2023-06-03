02aa729b59935d6ee4b149c9fefa38e8334298af
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-06-03 20:56:51 +0530

    corrected the login page css

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -4,16 +4,15 @@ import './Auth.css';
 
 const Auth = (props) => {
   return (
-    <div className='main'>
+    <section className='main'>
       <div className='hero'>
         <h1>Welcome to FinTrack</h1>
         <p>Here You Can Track Your Personal Finance and Improve Your Money Managing Skills</p>
       </div>
       <div className='form'>
-        <div className='wave'></div>
         <LoginForm setIsAuth = {props.setIsAuth} />
       </div>
-    </div>
+    </section>
   );
 };
 
